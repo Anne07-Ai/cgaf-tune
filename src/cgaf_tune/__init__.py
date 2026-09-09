824e2d29ec1c4c653dd7e6398afbd1c214795ae1
Lakshmi Anne <lakshmianne07@gmail.com>
2026-09-09 21:32:53 +0100

    feat: initialize cgaf_tune package

---
+++ src/cgaf_tune/__init__.py
@@ -0,0 +1,6 @@
+"""CGAF-Tune research prototype."""
+
+from .projection import CGAFConfig, ProjectionStats, apply_cgaf_projection
+
+__all__ = ["CGAFConfig", "ProjectionStats", "apply_cgaf_projection"]
+__version__ = "0.1.0"
