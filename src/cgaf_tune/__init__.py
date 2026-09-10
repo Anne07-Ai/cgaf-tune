06fdde78d88a61f5cfaf69a2f6e320e9c3ab1e3e
Lakshmi Anne <lakshmianne07@gmail.com>
2026-09-10 08:07:32 +0100

    Phase 1: update src/cgaf_tune/__init__.py

--- src/cgaf_tune/__init__.py
+++ src/cgaf_tune/__init__.py
@@ -1,6 +1,13 @@
 """CGAF-Tune research prototype."""
 
 from .projection import CGAFConfig, ProjectionStats, apply_cgaf_projection
+from .training import CGAFStepEngine, StepResult
 
-__all__ = ["CGAFConfig", "ProjectionStats", "apply_cgaf_projection"]
+__all__ = [
+    "CGAFConfig",
+    "CGAFStepEngine",
+    "ProjectionStats",
+    "StepResult",
+    "apply_cgaf_projection",
+]
 __version__ = "0.1.0"
